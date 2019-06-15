37db812a06c2f224fd05f3a06653f2e88e3aa5eb
cardluck <51752953+cardluck@users.noreply.github.com>
2019-06-15 10:08:18 +0800

    Update HelloWorld.java
    456

--- egit-demo/src/com/xmjava/demo/HelloWorld.java
+++ egit-demo/src/com/xmjava/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package com.xmjava.demo;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		//test git 
+		//test git456 
 		System.out.println("hello git");
 		System.out.println("hello git2");
 		System.out.println("hello git3"); //hi test push
